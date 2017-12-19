7ef63beb3cc20ed58c7d9d45af8bf635dea822f4
monzag <zaganiacz.m@gmail.com>
2017-12-19 15:00:55 +0100

    Refactor queries

--- src/main/resources/db/migration/V2.01__insert_customers.sql
+++ src/main/resources/db/migration/V2.01__insert_customers.sql
@@ -1,6 +1,6 @@
-INSERT INTO `customers` (name, surname, email) VALUES ('Maciej','Nowak','macius@gmail.com');
-INSERT INTO `customers` (name, surname, email) VALUES ('Bolec','Golec', 'bolec@gmail.com');
-INSERT INTO `customers` (name, surname, email) VALUES ('Bolec-brat','Golecki','bratBolec@wp.pl');
-INSERT INTO `customers` (name, surname, email) VALUES ('Janusz','Kowalski', 'januszek@onet.pl');
-INSERT INTO `customers` (name, surname, email) VALUES ('Gruby','Pasibrzuch','gruby@pasibrzuch.pl');
+INSERT INTO customers (name, surname, email) VALUES ('Maciej','Nowak','macius@gmail.com');
+INSERT INTO customers (name, surname, email) VALUES ('Bolec','Golec', 'bolec@gmail.com');
+INSERT INTO customers (name, surname, email) VALUES ('Bolec-brat','Golecki','bratBolec@wp.pl');
+INSERT INTO customers (name, surname, email) VALUES ('Janusz','Kowalski', 'januszek@onet.pl');
+INSERT INTO customers (name, surname, email) VALUES ('Gruby','Pasibrzuch','gruby@pasibrzuch.pl');
 
---
+++ src/main/resources/db/migration/V2.03__insert_orders.sql
@@ -0,0 +1,6 @@
+INSERT INTO orders (amount, client_id, dish_id) VALUES (1, 1, 1);
+INSERT INTO orders (amount, client_id, dish_id) VALUES (1, 2, 1);
+INSERT INTO orders (amount, client_id, dish_id) VALUES (1, 3, 3);
+INSERT INTO orders (amount, client_id, dish_id) VALUES (2, 4, 6);
+INSERT INTO orders (amount, client_id, dish_id) VALUES (4, 5, 4);
+INSERT INTO orders (amount, client_id, dish_id) VALUES (1, 2, 5);
