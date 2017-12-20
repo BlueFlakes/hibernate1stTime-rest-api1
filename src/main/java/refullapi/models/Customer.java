7047b772c09af25847a6a71f810259ed6e3044b8
Kamil <blueflamesxcode@gmail.com>
2017-12-20 03:14:39 +0100

    add config for hibernate

--- src/main/java/refullapi/models/Customer.java
+++ src/main/java/refullapi/models/Customer.java
@@ -3,18 +3,53 @@ package refullapi.models;
 import lombok.Getter;
 
 @Getter
-public class Customer {
+public class XD {
     private Integer id;
     private String name;
     private String surname;
     private String email;
 
-    public Customer() {}
+    private XD(CustomerBuilder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.surname = builder.surname;
+        this.email = builder.email;
+    }
 
-    public Customer(Integer id, String name, String surname, String email) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
+    public static CustomerBuilder builder() {
+        return new CustomerBuilder();
+    }
+
+    public static class CustomerBuilder {
+        private Integer id;
+        private String name;
+        private String surname;
+        private String email;
+
+        private CustomerBuilder() {}
+
+        public CustomerBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public CustomerBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public CustomerBuilder surname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public CustomerBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public XD build() {
+            return new XD(this);
+        }
     }
 }
