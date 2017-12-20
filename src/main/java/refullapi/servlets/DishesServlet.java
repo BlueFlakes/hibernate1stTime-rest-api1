5a46877b34f1d22d1cf1d76aa4df4284f1fa6ab6
monzag <zaganiacz.m@gmail.com>
2017-12-20 17:13:16 +0100

    Add doPost with DaoPool

--- src/main/java/refullapi/servlets/DishesServlet.java
+++ src/main/java/refullapi/servlets/DishesServlet.java
@@ -2,6 +2,8 @@ package refullapi.servlets;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.logging.log4j.core.util.IOUtils;
 import refullapi.hibernate.DaoPool;
 import refullapi.models.Dish;
 
@@ -11,13 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DishesServlet extends HttpServlet {
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         String path = req.getRequestURI().substring(req.getContextPath().length());
-        ObjectMapper mapper = new ObjectMapper();
         String jsonToString;
 
         try {
@@ -30,11 +34,11 @@ public class DishesServlet extends HttpServlet {
             }
 
             resp.setContentType("application/json");
+            resp.setStatus(200);
             resp.getWriter().write(jsonToString);
 
         } catch (Exception e) {
             resp.setStatus(404);
-            resp.getWriter().println("Fail: " + resp.getStatus());
         }
     }
 
@@ -48,19 +52,25 @@ public class DishesServlet extends HttpServlet {
         Dish dish = DaoPool.dishDao.get(Dish.class, id);
         return mapper.writeValueAsString(dish);
     }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String name = req.getParameter("name");
-//        Double price = Double.parseDouble(req.getParameter("price"));
-//        Dish dish = Dish.builder().name(name)
-//                                  .price(price)
-//                                  .build();
-//        dishDao.insert(dish);
-//        resp.setStatus(201);
-//        resp.getWriter().write("create");
-//    }
-//
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+        try {
+            String requestStr = req.getReader().lines().collect(Collectors.joining());
+            Dish dish = mapper.readValue(requestStr, Dish.class);
+            DaoPool.dishDao.saveToDatabase(dish);
+            resp.setStatus(201);
+            resp.getWriter().write("create");
+
+        } catch (InvalidFormatException e) {
+            resp.setStatus(406);
+
+        } catch (IOException e) {
+            resp.setStatus(400);
+        }
+    }
+
 //    @Override
 //    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //
