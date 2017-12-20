51bd51163114d20c38f4db24f332871d50b9ad7e
monzag <zaganiacz.m@gmail.com>
2017-12-20 21:17:20 +0100

    Remove useles files

--- src/main/resources/db/migration/V2.02__insert_dishes.sql
+++ src/main/resources/db/migration/V2.02__insert_dishes.sql
@@ -1,6 +1,6 @@
-INSERT INTO dishes (name, price) VALUES ('Pierogi dla chudych', 10);
-INSERT INTO dishes (name, price) VALUES ('Naleśniki z serową chmurką', 12);
-INSERT INTO dishes (name, price) VALUES ('Hindus, że aż w gardle piecze',20);
-INSERT INTO dishes (name, price) VALUES ('Soczysty kotlet schabowy', 13);
-INSERT INTO dishes (name, price) VALUES ('Big Tost', 5);
-INSERT INTO dishes (name, price) VALUES ('Makaron z czosnkiem na wampiry', 11);
+INSERT INTO dishes (name, price) VALUES ('Pierogi dla chudych', 10.1);
+INSERT INTO dishes (name, price) VALUES ('Naleśniki z serową chmurką', 12.0);
+INSERT INTO dishes (name, price) VALUES ('Hindus, że aż w gardle piecze',20.0);
+INSERT INTO dishes (name, price) VALUES ('Soczysty kotlet schabowy', 13.5);
+INSERT INTO dishes (name, price) VALUES ('Big Tost', 5.0);
+INSERT INTO dishes (name, price) VALUES ('Makaron z czosnkiem na wampiry', 11.1);
--- src/main/resources/db/migration/V2.03__insert_dishes.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `dishes` (name, price) VALUES ('Pierogi dla chudych', 10);
-INSERT INTO `dishes` (name, price) VALUES ('Naleśniki z serową chmurką', 12);
-INSERT INTO `dishes` (name, price) VALUES ('Hindus, że aż w gardle piecze',20);
-INSERT INTO `dishes` (name, price) VALUES ('Soczysty kotlet schabowy', 13);
-INSERT INTO `dishes` (name, price) VALUES ('Big Tost', 5);
-INSERT INTO `dishes` (name, price) VALUES ('Makaron z czosnkiem na wampiry', 11);
